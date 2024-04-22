c50452b2948a728e304e772d7a87cdee3e4bcd3a
mariog44 <mg441236@gmail.com>
2024-04-22 16:29:16 -0500

    Se agregaron controles a la vista de SignIn

---
+++ wwwroot/js/SignIn.js
@@ -0,0 +1,20 @@
+﻿const UserPassword = document.getElementById('UserPassword');
+const UserPasswordConfirm = document.getElementById('UserPasswordConfirm');
+const RegisterButton = document.getElementById('RegisterButton');
+
+UserPassword.addEventListener('input', ValidatePassword);
+UserPasswordConfirm.addEventListener('input', ValidatePassword);
+
+function ValidatePassword() {
+    const UserPasswordVariable = UserPassword.value;
+    const UserPasswordConfirmVariable = UserPasswordConfirm.value;
+
+    if ((UserPasswordVariable.length) >= 8 && (UserPasswordVariable === UserPasswordConfirmVariable)) {
+        RegisterButton.disabled = false;
+        RegisterButton.classList.add("RegisterButtonEnabled");
+
+    } else {
+        RegisterButton.disabled = true;
+        RegisterButton.classList.remove("RegisterButtonEnabled");
+    }
+}
